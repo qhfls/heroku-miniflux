679ba0b72b1e77ceef562c0474515e2a5dfb8b14
lefty <lefty1982@gmail.com>
2021-02-20 10:10:21 +0800

    Update heroku-cli.sh

--- heroku-cli.sh
+++ heroku-cli.sh
@@ -2,8 +2,9 @@
 
 heroku apps:create
 heroku addons:create heroku-postgresql:hobby-dev
+heroku config:set HTTPS=1
 heroku config:set RUN_MIGRATIONS=1
 heroku config:set CREATE_ADMIN=1
 heroku config:set ADMIN_USERNAME=admin
-heroku config:set ADMIN_PASSWORD=test123
+heroku config:set ADMIN_PASSWORD=admin9527
 git push heroku main
